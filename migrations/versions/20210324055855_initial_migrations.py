3c224594b14da28ac6a52bd12c7fa2f72e283176
Kyrylo Petrov <bjakushka@gmail.com>
2021-03-24 09:15:55 +0300

    Create file to keeping all initial db-migrations
    See: #5

--- migrations/versions/20210324055855_initial_migrations.py
+++ migrations/versions/20210324055855_initial_migrations.py
@@ -1,15 +1,15 @@
-"""Create table `Users`
+"""Initial migrations
 
-Revision ID: 48458f4d7701
-Revises:
-Create Date: 2020-12-09 10:03:37.375419+00:00
+Revision ID: 2ce4a8dfd6b9
+Revises: 
+Create Date: 2021-03-24 05:58:55.498166+00:00
 
 """
 from alembic import op
 import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
-revision = '48458f4d7701'
+revision = '2ce4a8dfd6b9'
 down_revision = None
 branch_labels = None
 depends_on = None
