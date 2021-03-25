ad0fdc249b980c16aedae2bc85138242cfb84500
Kyrylo Petrov <bjakushka@gmail.com>
2021-03-25 09:52:21 +0300

    Define rules of keys naming for SQLAlchemy
    See: #5

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,24 @@
-"""Main entry point to the application"""
+'''Main entry point to the application'''
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_restful import Api
+from sqlalchemy import MetaData
 from .config import Loader as ConfigLoader
 
 app = Flask(__name__, static_folder='../public')
 app.config.update(ConfigLoader().get_config())
-db = SQLAlchemy(app)
+
+# initialize plugins and extensions
+convention = {
+    'ix': '%(table_name)s   _ix',
+    'uq': '%(table_name)s_uq_%(column_0_name)s',
+    'fk': '%(table_name)s_fk_%(referred_table_name)s',
+    'pk': '%(table_name)s_pk',
+}
+metadata = MetaData(naming_convention=convention)
+db = SQLAlchemy(app, metadata=metadata)
 migrate = Migrate(app, db)
 api = Api(app)
 
